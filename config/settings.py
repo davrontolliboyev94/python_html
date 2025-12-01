4c2d13c1aa86fdca4fc2f2e21c157188fcdc90ae
davrontolliboyev94@gmail.com <you@example.com>
2025-12-01 19:01:10 +0500

    html+css

--- config/settings.py
+++ config/settings.py
@@ -43,11 +43,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.contrib.staticfiles',
+
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -115,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   # Development uchun
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"   # collectstatic shu yerga yig‘adi
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
